c34eca0ea6d1a54b78c9a1e8b3fda65c0eefd991
Twan Koolen <koolen.twan@gmail.com>
2016-05-27 12:08:51 -0400

    parse example urdf

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 using IJulia
+using RigidBodyTreeInspector
+
+let
+    acrobot_vis = parse_urdf("urdf/Acrobot.urdf")
+end
+
 jupyter = IJulia.jupyter
 notebook = "../examples/demo.ipynb"
 run(`$jupyter nbconvert --to notebook --execute $notebook --output $notebook`)
